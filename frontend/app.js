434aac0ecff833dd9a4b954b8031a3c44b075c69
Enem_Flow <kayky.ys29@gmail.com>
2026-05-15 20:24:13 -0300

    Resolvendo problema de conexão

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,7 @@
+const protocol = window.location.protocol;
 const host = window.location.hostname;
-const API_URL = window.location.port === '5500' || host === 'localhost' || host === '127.0.0.1' || host.startsWith('192.168.')
-  ? `http://${host}:3000`
-  : ''; // Vazio significa que ele usará o mesmo domínio que está servindo o site
+const isLocal = protocol === 'file:' || window.location.port === '5500' || host === 'localhost' || host === '127.0.0.1' || host.startsWith('192.168.');
+const API_URL = isLocal ? `http://${host || 'localhost'}:3000` : '';
 const App = {
   getToken: () => localStorage.getItem('token'),
   setToken: (token) => localStorage.setItem('token', token),
